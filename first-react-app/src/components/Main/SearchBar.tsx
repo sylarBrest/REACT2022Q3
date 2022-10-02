c3d10d658206a028881ba3ff4d99a6e6152fd04f
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-10-03 00:11:38 +0300

    feat: load value from localStorage

--- first-react-app/src/components/Main/SearchBar.tsx
+++ first-react-app/src/components/Main/SearchBar.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { TSearchBarProps, TSearchBarState } from 'types';
 
 class SearchBar extends React.Component<TSearchBarProps, TSearchBarState> {
   constructor(props: TSearchBarProps) {
     super(props);
     this.state = {
-      value: '',
+      value: localStorage.getItem('searchBarValue') || '',
     };
+  }
+
+  componentDidMount() {
+    this.setState({ value: localStorage.getItem('searchBarValue') || '' });
   }
 
   componentWillUnmount() {
     localStorage.setItem('searchBarValue', this.state.value);
   }
 
+  saveValueOnChange = (event: ChangeEvent) => {
+    this.setState({ value: (event.target as HTMLInputElement).value || '' });
+  };
+
   render() {
     return (
-      <input type="search" placeholder={this.props.placeholder} value={this.state.value}></input>
+      <input
+        type="search"
+        className="input"
+        placeholder={this.props.placeholder}
+        defaultValue={this.state.value}
+        onChange={this.saveValueOnChange}
+      />
     );
   }
 }
