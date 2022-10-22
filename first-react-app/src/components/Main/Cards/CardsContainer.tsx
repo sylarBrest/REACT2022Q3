c8009a7b3a6984c2530190ba35c6a1ebcd96a552
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-10-22 23:42:58 +0300

    init: start react-api task, convert CardsContainer to class

--- first-react-app/src/components/Main/Cards/CardsContainer.tsx
+++ first-react-app/src/components/Main/Cards/CardsContainer.tsx
@@ -1,15 +1,18 @@
+import React from 'react';
 import Card from './Card';
 import { MOVIES_DATA } from 'data/constants';
 import { CardPropsType } from 'data/types';
 
-const CardsContainer = () => {
-  return (
-    <div className="cards" data-testid="cards-container">
-      {MOVIES_DATA.map((movieData: CardPropsType) => (
-        <Card {...movieData} key={movieData.title.replace(/\s/g, '-').toLowerCase()} />
-      ))}
-    </div>
-  );
-};
+class CardsContainer extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <div className="cards" data-testid="cards-container">
+        {MOVIES_DATA.map((movieData: CardPropsType) => (
+          <Card {...movieData} key={movieData.title.replace(/\s/g, '-').toLowerCase()} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default CardsContainer;
