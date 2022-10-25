7a33202f7c0de46e4c683955067e5930daf7132e
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-10-25 15:57:04 +0300

    fix: fix tests wick mock api

--- first-react-app/src/components/Main/Cards/Card.test.tsx
+++ first-react-app/src/components/Main/Cards/Card.test.tsx
@@ -1,27 +1,67 @@
-import { act, fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Card from 'components/Main/Cards/Card';
 import SearchWrapper from '../SearchWrapper';
+import { SearchData } from 'data/types';
+
+const mockData: SearchData = {
+  hits: [
+    {
+      collections: 123,
+      comments: 2,
+      downloads: 1000,
+      id: 123456,
+      imageHeight: 500,
+      imageSize: 3456737,
+      imageWidth: 1000,
+      largeImageURL: 'path/to/large/image',
+      likes: 300,
+      pageURL: 'path/to/page/with/image',
+      previewHeight: 40,
+      previewURL: 'path/to/preview',
+      previewWidth: 80,
+      tags: 'cars, test',
+      type: 'image',
+      user: 'sylarBrest',
+      userImageURL: 'path/to/user/avatar',
+      user_id: 98765,
+      views: 2022,
+      webformatHeight: 320,
+      webformatURL: 'path/to/webformat/image',
+      webformatWidth: 640,
+    },
+  ],
+  total: 100,
+  totalHits: 20,
+};
 
 describe('Card component', () => {
-  it('should render onto the screen if rating and voices present', async () => {
-    act(() => {
-      render(<SearchWrapper />);
-    });
+  let originalFetch: (
+    input: RequestInfo | URL,
+    init?: RequestInit | undefined
+  ) => Promise<Response>;
+
+  beforeEach(() => {
+    originalFetch = global.fetch;
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockData),
+      })
+    ) as jest.Mock;
+  });
+
+  afterEach(() => {
+    global.fetch = originalFetch;
+  });
+
+  it('should render onto the screen api call was successful', async () => {
+    render(<SearchWrapper />);
     const searchInput = screen.getByTestId('search-bar') as HTMLInputElement;
     expect(searchInput).toBeInTheDocument();
     userEvent.type(searchInput, 'ferrari');
     expect(searchInput).toHaveValue('ferrari');
-    act(() => {
-      fireEvent.keyPress(searchInput, { key: 'Enter', charCode: 13 });
-    });
+    fireEvent.keyPress(searchInput, { key: 'Enter', charCode: 13 });
     expect(searchInput).toHaveValue('');
     expect(screen.getByText('Loading...')).toBeInTheDocument();
     expect(await screen.findByTestId(`card-123456`)).toBeInTheDocument();
   });
-
-  /*   it('should render onto the screen if rating and voices not present', () => {
-    render(<Card {...mockMovieData[1]} />);
-    expect(screen.getByTestId(`card-${mockMovieData[1].id}`)).toBeInTheDocument();
-  }); */
 });
--- first-react-app/src/setupTests.ts
+++ first-react-app/src/setupTests.ts
@@ -1,11 +1,5 @@
 import '@testing-library/jest-dom';
-import { SearchData } from 'data/types';
-// import server from './mocks/server';
 
-/* beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
- */
 const localStorageMock = (function () {
   let store: { [key: string]: string } = {};
 
@@ -33,44 +27,3 @@ const localStorageMock = (function () {
 })();
 
 Object.defineProperty(window, 'localStorage', { value: localStorageMock });
-
-const mockData: SearchData = {
-  hits: [
-    {
-      collections: 123,
-      comments: 2,
-      downloads: 1000,
-      id: 123456,
-      imageHeight: 500,
-      imageSize: 3456737,
-      imageWidth: 1000,
-      largeImageURL: 'path/to/large/image',
-      likes: 300,
-      pageURL: 'path/to/page/with/image',
-      previewHeight: 40,
-      previewURL: 'path/to/preview',
-      previewWidth: 80,
-      tags: 'cars, test',
-      type: 'image',
-      user: 'sylarBrest',
-      userImageURL: 'path/to/user/avatar',
-      user_id: 98765,
-      views: 2022,
-      webformatHeight: 320,
-      webformatURL: 'path/to/webformat/image',
-      webformatWidth: 640,
-    },
-  ],
-  total: 100,
-  totalHits: 20,
-};
-
-beforeEach(() => {
-  return jest.spyOn(global, 'fetch').mockResolvedValue({
-    json: jest.fn().mockResolvedValue(mockData),
-  });
-});
-
-afterEach(() => {
-  jest.restoreAllMocks();
-});
